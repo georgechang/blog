cdeebc2bc3a95affe82b65c627d05d011fb84a70
George Chang <changg@gmail.com>
2020-09-25 21:18:13 -0400

    Add Tailwind CSS and update styles

---
+++ themes/gc/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: true,
+    ['./layouts/**/*.html']
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
