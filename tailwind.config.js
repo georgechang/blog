d045b1873c25d81a201d70ee8f9f81098acfd4a0
George Chang <changg@gmail.com>
2020-10-01 21:29:42 -0400

    Add Netlify configuration and move typography modifications to the Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,42 @@ module.exports = {
     }
   },
   theme: {
+    typography: {
+      default: {
+        css : {
+          pre: {
+            marginTop: '0',
+            marginBottom: '0',
+            maxWidth: 'calc(100vw - 2rem)'
+          },
+          ol: {
+            marginTop: '0',
+            marginBottom: '0',
+          },
+          a: {
+              wordBreak: 'break-all'
+            },
+          h2: {
+            marginTop: '1rem'
+          },
+
+          'code::before': {
+            content: ''
+          },
+
+          'code::after': {
+            content: ''
+          },
+
+          'blockquote p:first-of-type::before': {
+            content: ''
+          },
+          'blockquote p:last-of-type::after': {
+            content: ''
+          },
+        }
+      }
+    },
     extend: {
       maxWidth: {
         '1/3': '33%',
