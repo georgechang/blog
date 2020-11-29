2ad159c23ae6bc5dba915546029141fa8a99c0b2
George Chang <changg@gmail.com>
2020-11-29 14:48:13 -0500

    Update to Tailwind 2.0 and updated typography module

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,54 +1,49 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: {
-    mode: 'all',
-    content: ['./layouts/**/*.html'],
-    options: {
-      whitelist: ['pagination', 'page-item', 'page-link', 'disabled', 'active'],
-    }
-  },
+  // purge: {
+  //   content: ['./layouts/**/*.html'],
+  //   options: {
+  //     safelist: ['pagination', 'page-item', 'page-link', 'disabled', 'active'],
+  //   }
+  // },
   theme: {
-    typography: {
-      default: {
-        css : {
-          maxWidth: '72ch',
-          pre: {
-            marginTop: '0',
-            marginBottom: '0',
-            maxWidth: 'calc(100vw - 2rem)'
-          },
-          ol: {
-            marginTop: '0',
-            marginBottom: '0',
-          },
-          a: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '72ch',
+            pre: {
+              marginTop: '0',
+              marginBottom: '0',
+              maxWidth: 'calc(100vw - 2rem)'
+            },
+            ol: {
+              marginTop: '0',
+              marginBottom: '0',
+            },
+            a: {
               wordBreak: 'break-word'
             },
-          h2: {
-            marginTop: '1rem'
-          },
+            h2: {
+              marginTop: '1rem'
+            },
 
-          'code::before': {
-            content: ''
-          },
+            'code::before': {
+              content: ''
+            },
 
-          'code::after': {
-            content: ''
-          },
+            'code::after': {
+              content: ''
+            },
 
-          'blockquote p:first-of-type::before': {
-            content: ''
-          },
-          'blockquote p:last-of-type::after': {
-            content: ''
-          },
+            'blockquote p:first-of-type::before': {
+              content: ''
+            },
+            'blockquote p:last-of-type::after': {
+              content: ''
+            },
+          }
         }
-      }
-    },
-    extend: {
+      },
       maxWidth: {
         '1/3': '33%',
         '1/2': '50%',
@@ -60,7 +55,6 @@ module.exports = {
       }
     },
   },
-  variants: {},
   plugins: [
     require('@tailwindcss/typography'),
   ],
